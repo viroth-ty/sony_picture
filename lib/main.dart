eca9aaa68290f1b22aa09b5725d5359a540fddd9
viroth <viroth.ty@codingate.com>
2021-12-10 11:09:43 +0700

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
